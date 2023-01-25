8fddd324ea7fbd42fb92bbc2f1b6b24688016ef8
Frank Aragona <frank.aragona@doh.wa.gov>
2023-01-25 12:39:26 -0800

    update env conda

--- a_script.R
+++ a_script.R
@@ -1,7 +1,15 @@
 library(dplyr)
 library(lubridate)
 library(janitor)
+library(reticulate)
 
 Titanic %>%
   as_tibble() %>%
   group_by(Age)
+
+# python part 
+reticulate::repl_python()
+
+import requests
+
+exit
